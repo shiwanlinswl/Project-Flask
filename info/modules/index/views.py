627e8b9a11ee9df7ec77a1c9dabd8ae62c54c81c
swl <swl@163.com>
2019-12-26 07:47:51 +0800

    提交新闻首页接口

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,7 +1,8 @@
 from . import index_bp
 from flask import jsonify
+from flask import render_template
 
 
 @index_bp.route('/')
 def index():
-    return jsonify(msg="Hello World")
+    return render_template("news/index.html")
