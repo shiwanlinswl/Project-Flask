5ca252cea135e7c774dbb676f09e99e939a988da
swl <swl@163.com>
2020-02-18 21:59:08 +0800

    查询用户基本资料并展示

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,6 +1,21 @@
-from flask import render_template, g
+from flask import render_template, g, request
 from . import profile_bp
 from info.utils.common import get_user_data
+
+
+@profile_bp.route("/base_info", methods=["POST", "GET"])
+@get_user_data
+def user_base_info():
+    """
+    基本资料
+    :return:
+    """
+    if request.method == "GET":
+        user = g.user
+        data = {
+            "user_info": user.to_dict() if user else None
+        }
+        return render_template("profile/user_base_info.html", data=data)
 
 
 @profile_bp.route("/info")
