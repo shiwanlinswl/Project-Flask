1513ad735a1394945ff7c1b534ee064690443c04
swl <swl@163.com>
2020-02-18 21:37:37 +0800

    个人中心用户数据展示

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,12 +1,17 @@
-from flask import render_template
+from flask import render_template, g
 from . import profile_bp
+from info.utils.common import get_user_data
 
 
 @profile_bp.route("/info")
+@get_user_data
 def user_info():
     """
     个人中心
     :return:
     """
-    data = {}
+    user = g.user
+    data = {
+        "user_info": user.to_dict() if user else None
+    }
     return render_template("profile/user.html", data=data)
